7b5b479690c91afe56d3ecc029044b2ac39d423a
Ashok <ashok.desai.90@gmail.com>
2021-09-05 22:46:12 +0530

    New groovy File DSL

--- MavenProjectDSL.groovy
+++ MavenProjectDSL.groovy
@@ -7,7 +7,6 @@ job('First-Maven-Project-Via-DSL') {
         scm('* * * * *')
     }
     steps {
-        mavenInstallation('LocalMaven')
         maven('clean package', 'maven-samples/single-module/pom.xml')
     }
     publishers {
